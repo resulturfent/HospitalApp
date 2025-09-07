42652e42e08e9e1b63f7d5a81958ae27478b4dff
resulturfent <rsltrfnt@gmail.com>
2025-09-07 19:05:56 +0300

    data eklemesi

--- HospitalApp/Entity/Clinic.cs
+++ HospitalApp/Entity/Clinic.cs
@@ -3,6 +3,21 @@
 internal class Clinic:BaseEntity
 {
     public int Id { get; set; }
-    public string Name { get; set; }    
+    public string Name { get; set; }
 
+
+    public List<Clinic> List()
+    {
+        var clinics = new List<Clinic>
+        {
+            new Clinic { Id = 1, Name = "KBB",IsActive=true,CreateDate=DateTime.Now },
+            new Clinic { Id = 2, Name = "Göz",IsActive=true },
+            new Clinic { Id = 3, Name = "Nöroloji",IsActive=true },
+            new Clinic { Id = 4, Name = "Embriyoloji" , IsActive = false},
+            new Clinic { Id = 5, Name = "Dahiliye" , IsActive = true}
+        };
+
+        return clinics;
+
+    }
 }
