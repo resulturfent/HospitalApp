08d6fcab3ffcdc8082241ee8a6a7701917893bc3
resulturfent <rsltrfnt@gmail.com>
2025-09-07 19:57:51 +0300

    bug

--- HospitalApp/Entity/DeleteBaseEntity.cs
+++ HospitalApp/Entity/DeleteBaseEntity.cs
@@ -4,4 +4,9 @@ internal class DeleteBaseEntity:BaseEntity
 {
     public DateTime DeleteDate { get; set; }
     public int DeleterId { get; set; }
+
+    public override void DeleteInfo(int deleterId)
+    {
+        throw new NotImplementedException();
+    }
 }
